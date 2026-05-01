702788869db2de5caf76d6f25230c12c1272439a
Cheef44 <cheef44.prog@gmail.com>
2026-05-01 21:23:21 +0300

    feature: реализация смерти и переноса света на объект управления

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -1,12 +1,14 @@
 extends CharacterBody2D
 
 const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const JUMP_VELOCITY = -310.0
 
 var current_form: Transformable = null   # объект в котором сидим
 var nearby_form: Transformable = null
 @onready var camera: Camera2D = $Camera2D
 @onready var ui_player: Control = $"UI Player"
+@onready var point_light_2d: PointLight2D = $PointLight2D
+
 var max_energy = 100
 
 func _ready() -> void:
@@ -16,7 +18,9 @@ func _physics_process(delta: float) -> void:
 	if current_form != null:
 		# Управление объектом
 		current_form.velocity = current_form.get_vector()
+		var pre_slide_velocity = current_form.velocity
 		current_form.move_and_slide()
+		current_form.apply_bounce(pre_slide_velocity)
 	else:
 		if not is_on_floor():
 			velocity += get_gravity() * delta
@@ -55,14 +59,18 @@ func _enter_form(form: Transformable) -> void:
 	
 	# Камера переезжает к объекту
 	camera.reparent(form)
+	point_light_2d.reparent(form)
+	point_light_2d.position = Vector2.ZERO
 	camera.position = Vector2.ZERO  # центрируем на объекте
 
 func _exit_form() -> void:
 	# Игрок появляется рядом с объектом
-	position = current_form.position + Vector2(60, 0)
+	position = current_form.position + Vector2(0, -60)
 	
 	# Камера возвращается к игроку
 	camera.reparent(self)
+	point_light_2d.reparent(self)
+	point_light_2d.position = Vector2.ZERO
 	camera.position = Vector2.ZERO
 	
 	current_form.on_exit()
@@ -79,3 +87,6 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 	if body.has_method("hill"):
 		if energy < max_energy:
 			ui_player.update_energy(energy+body.hill(), max_energy)
+
+func die():
+	get_tree().reload_current_scene()
