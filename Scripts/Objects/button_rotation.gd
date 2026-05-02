70c10e3846db7c8f65244f31a90fd53f0f267409
Cheef44 <cheef44.prog@gmail.com>
2026-05-02 22:30:35 +0300

    feature: реализация логики кнопки вращения

---
+++ Scripts/Objects/button_rotation.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+@export var platform: Array[NodePath]
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	for path in platform:
+		get_node(path).start_rotation()
+
+
+func _on_body_exited(body: Node2D) -> void:
+	for path in platform:
+		get_node(path).stop_rotation()
