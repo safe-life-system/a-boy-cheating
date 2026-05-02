09d97a2a33677c3adfc0f3ea2f93bf6f2d222db6
Cheef44 <cheef44.prog@gmail.com>
2026-05-02 14:17:12 +0300

    feature: реализация добавления ключа в UI

--- Scripts/UI/UI_player.gd
+++ Scripts/UI/UI_player.gd
@@ -1,5 +1,7 @@
 extends Control
 @onready var energy_bar: TextureProgressBar = $CanvasLayer/MarginContainer/VBoxContainer/HBoxContainer/EnergyBar
+@onready var label: Label = $CanvasLayer/MarginContainer/VBoxContainer/HBoxContainer/Sprite2D/Label
+
 
 func _ready() -> void:
 	energy_bar.value = 100.0
@@ -8,3 +10,6 @@ func _ready() -> void:
 func update_energy(current: float, max_energy: float) -> void:
 	energy_bar.max_value = max_energy
 	energy_bar.value = current
+
+func _physics_process(delta: float) -> void:
+	label.text = "X"+str(Global.leave_key)
