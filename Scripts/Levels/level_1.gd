b8fb31725854b25adad6567fec85c93748e3076a
Cheef44 <cheef44.prog@gmail.com>
2026-05-03 13:28:11 +0300

    feature: реализация спавна игрока. Реализация проигрования музыки уровня. Отключение теней на мобильных устройствах

--- Scripts/Levels/level_1.gd
+++ Scripts/Levels/level_1.gd
@@ -1,13 +1,25 @@
 extends Node2D
 
+@onready var player: CharacterBody2D = $Player
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	var spawn_name = Global.spawn_point
+	if has_node(spawn_name):
+		player.position = get_node(spawn_name).position
 	$Button.presed.connect($SifiDoor.open)
 	$Button.relised.connect($SifiDoor.close)
-
+	if not Global.menu:
+		$AudioStreamPlayer.autoplay = true
+		$AudioStreamPlayer.play()
+	if OS.has_feature("mobile"):
+		for light in get_tree().get_nodes_in_group("lights"):
+			light.shadow_enabled = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_audio_stream_player_finished() -> void:
+	$AudioStreamPlayer.play()
