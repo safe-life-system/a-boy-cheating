1b3d1555fc39aa72335b5bfcb7dfbc0267138f87
Cheef44 <cheef44.prog@gmail.com>
2026-05-03 13:43:00 +0300

    feature: включение UI дя мобильных устройств только на мобильных устройствах

--- Scripts/UI/UI_player.gd
+++ Scripts/UI/UI_player.gd
@@ -6,6 +6,8 @@ extends Control
 func _ready() -> void:
 	energy_bar.value = 100.0
 	energy_bar.max_value = 100.0
+	if not OS.has_feature("mobile"):
+		$Mobile.visible = false
 # Вызывается из Player когда энергия меняется
 func update_energy(current: float, max_energy: float) -> void:
 	energy_bar.max_value = max_energy
